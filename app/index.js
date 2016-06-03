3a73af1e55db1f9266b502a57028384f7be7d45b
Bruno S Reis <bruno.reis@useboom.net>
2016-06-02 22:37:26 -0300

    added Avatar component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+  name: 'Bruno',
+  username: 'bruno-reis',
+  image: 'https://upload.wikimedia.org/wikipedia/en/a/a3/TMNTMichelangelo2012.png'
+}
+
 var React = require('react');
 var ReacrDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div> Hello React World </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReacrDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
