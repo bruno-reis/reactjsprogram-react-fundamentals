1affbd903484497bce7318aba9df85954ec0cc56
Bruno S Reis <bruno.reis@useboom.net>
2016-06-05 13:20:38 -0300

    added Prompt component/Refactored container as stateless

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,21 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var transparentBg = require('../styles').transparentBg;
 
 var Home = React.createClass({
   render: function() {
     "use strict";
     return (
-      <div> Hello from home!</div>
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>Github Battle</h1>
+        <p className="lead">Motto</p>
+        <Link to='/playerOne'>
+          <button type="button" className="btn btn-large btn-success">
+            Start!
+          </button>
+        </Link>
+      </div>
     )
   }
 });
